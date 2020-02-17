dce9273316bf686c6920ea472478ac5344d8128e
JyriKa <jyri.jk@gmail.com>
2020-02-17 02:20:19 +0200

    fixed first time boot up

--- app/src/main/java/com/example/coordinator/MainActivity.java
+++ app/src/main/java/com/example/coordinator/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements PostAdapter.OnCar
         if (mAuth.getCurrentUser() == null) {
             signInAnonymously();
         }
-        else initCaller();
+        initCaller();
     }
 
     private void initCaller() {
